0a65d8469bffc3e777b5fcfbf1981d70f81cebea
Hannah. g. <hannagebrehiwet3@gmail.com>
2024-02-24 14:46:26 -0800

    comitted edit

--- FORUM_FRONTEND/evangadi_forum/src/assets/Components/Header/Header.jsx
+++ FORUM_FRONTEND/evangadi_forum/src/assets/Components/Header/Header.jsx
@@ -2,20 +2,21 @@ import classes from "./Header.module.css"
 import evangadi_logo from "../../images/evangadi.png"
 function Header() {
   return (
-    <section className={classes.header_container}>
-        <div>
-            <a href="#"><img src={evangadi_logo} alt="evangadi_logo"></img></a>
-        </div>
-        <div className={classes.header_links}>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">How it Works</a></li>
-                <li className={classes.header_signin}><a href="#">Sign In</a></li>
-            </ul>
-        </div>
-        <button className={classes.hamburger_menu}>&#9776;</button>
-    </section>
-  )
+		<section className={classes.header_container}>
+			<div className={classes.logo}>
+				<a href="#">
+					<img src={evangadi_logo} alt="evangadi_logo"></img>
+				</a>
+			</div>
+			<div className={classes.header_links}>
+				<ul>
+					<li>
+						<a href="#">Home</a>
+					</li>
+				</ul>
+			</div>
+		</section>
+	);
 }
 
 export default Header
