3e80aaf0d7ed3c749e39d409ab0c48c6532a568a
fanb12 <fantaneshbezabh2@gmail.com>
2024-02-25 19:15:32 +0300

    commit front

---
+++ FORUM_FRONTEND/evangadi_forum/src/Components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import classes from "./Header.module.css";
+import evangadi_logo from "../../images/evangadi.png";
+function Header() {
+  return (
+    <section className={classes.header_container}>
+      <div className={classes.logo}>
+        <a href="#">
+          <img src={evangadi_logo} alt="evangadi_logo"></img>
+        </a>
+      </div>
+      <div className={classes.header_links}>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
