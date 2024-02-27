6c6db42e5217624d39b1701afd346fc46fb8916b
hannah-gebrehiwet <hannagebrehiwet3@gmail.com>
2024-02-27 00:42:51 -0800

    question insertion

--- src/Routerr.jsx
+++ src/Routerr.jsx
@@ -6,22 +6,23 @@ import Landing from "./Components/Landing/Landing";
 import QuestionList from "./page/Allquestion/QuestionList";
 import Singlequetion from "./page/singleQuestion/Singlequetion";
 import Answer from "./page/answerEntry/answer";
-
+import Askquestion from "./page/askquestion/askquestion";
 function Routerr() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/SignUp" element={<Signup />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Home" element={<QuestionList />} />
-          <Route path="/Single" element={<Singlequetion />} />
-          <Route  path="/enteranswer" element={<Answer />}/>
-        </Routes>
-      </Router>
-    </div>
-  );
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/SignUp" element={<Signup />} />
+					<Route path="/Register" element={<Register />} />
+					<Route path="/Home" element={<QuestionList />} />
+					<Route path="/Single" element={<Singlequetion />} />
+					<Route path="/enteranswer" element={<Answer />} />
+					<Route path="/enterquestion" element={<Askquestion/>} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default Routerr;
